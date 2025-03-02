d7c2ed0dd5f83d48343f8c5b736e0b8bcd5f67a3
psi-xto <psixto@keemail.me>
2025-03-02 11:00:46 +0500

    cleanup

--- 13/13.py
+++ 13/13.py
@@ -8,3 +8,12 @@ for mask in range(10,32):
             c += 1
     if c == 10:
         print(mask)
+
+from ipaddress import *
+c = 0
+a = ip_network(f'10.48.96.0/255.255.240.0',0)
+for i in a:
+    b = f'{i:b}'
+    if b.count('1')>b.count('0'):
+        c += 1
+print(c)
--- main.py
+++
@@ -1,8 +0,0 @@
-from ipaddress import *
-c = 0
-a = ip_network(f'10.48.96.0/255.255.240.0',0)
-for i in a:
-    b = f'{i:b}'
-    if b.count('1')>b.count('0'):
-        c += 1
-print(c)
